e3931a7dafd7b0a62590583c27dedf7b7b6d96c3
Helder Siqueira <helder.erik.he@gmail.com>
2021-03-02 23:27:00 -0300

    Versão dia 02-03-2021

--- main.js
+++ main.js
@@ -4,20 +4,16 @@
 
 const distancia = document.getElementById('distancia');
 const qtdPedagios =  document.getElementById('pedagios');
-const calcular = document.getElementById('calcular');
 const pedagio = document.getElementById('pedagio');
-const total = document.getElementById('total')
+const total = document.getElementById('total');
+const calcular = document.getElementById('calcular');
 
-function calculaPedagio(){
-    const resultadoP = Number(qtdPedagios.value) * 9.40;
-    pedagio.innerText = resultadoP.toFixed(2);
+function calcula(){
+    const resultadoPedagios = Number(qtdPedagios.value) * 9.40;
+    pedagio.innerText = resultadoPedagios.toFixed(2);
+
+    const resultadoDistancia = Number(distancia.value) * 6.00;
+    total.innerText = (resultadoDistancia + resultadoPedagios).toFixed(2);
 }
 
-calcular.addEventListener('click', calculaPedagio);
-
-function calculoTotal(){
-    const resultadoTotal = Number(distancia.value) * 6.00;
-    total.innerText = resultadoTotal.toFixed(2);
-}
-
-calcular.addEventListener('click', calculoTotal);
+calcular.addEventListener('click', calcula);
